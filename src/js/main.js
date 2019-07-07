38353aa2a1e579261e534dfbf5e3f11a6af14423
SokolX <89soek@gmail.com>
2019-07-07 07:38:21 +0200

    Add content, css and scripts from old project

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,13 @@
 
 import moment from 'moment';
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+const navigationSwitcher = document.querySelector('.navigation__switcher--js');
+
+navigationSwitcher.addEventListener('click', (e) => {
+
+    const navigationList = document.querySelector('.navigation__list--js');
+    navigationList.classList.toggle('navigation__list--visible');
+    hamburgerFocus.classList.remove('navigation__switcher--js:focus', 'navigation__switcher--js:focus::after')
+});
